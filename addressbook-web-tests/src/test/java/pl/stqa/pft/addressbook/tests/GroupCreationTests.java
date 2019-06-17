22e17c680fbdfca1447040f9443d5284ae03c133
trokowicz <51273895+trokowicz@users.noreply.github.com>
2019-06-17 18:17:48 +0200

    Zrealizowano sprawdzenie i wykonanie warunków wstępnych dla testów grupy

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,10 +8,12 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("test2", null, null));
+    /*
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test2", null, null));
     app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().returnToGroupPage(); */
   }
 
 }
