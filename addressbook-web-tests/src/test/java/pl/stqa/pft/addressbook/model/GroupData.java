22a416842672b8f547466e522befd586fef5706a
trokowicz <51273895+trokowicz@users.noreply.github.com>
2019-06-27 22:01:48 +0200

    Searching for the maximum element in the collection - Group creation tests

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
@@ -3,23 +3,27 @@ package pl.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
-    public String getId() {
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
         return id;
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -43,7 +47,7 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) &&
+        return id == groupData.id &&
                 Objects.equals(name, groupData.name);
     }
 
