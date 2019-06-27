30020043c2e89306fe9a8256375f933be2706329
trokowicz <51273895+trokowicz@users.noreply.github.com>
2019-06-27 23:05:02 +0200

    Collection sorting - Group creation and modification tests

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
@@ -17,7 +17,7 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -43,25 +43,24 @@ public class GroupData {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return id == groupData.id &&
-                Objects.equals(name, groupData.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
     public String toString() {
         return "GroupData{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
 }
