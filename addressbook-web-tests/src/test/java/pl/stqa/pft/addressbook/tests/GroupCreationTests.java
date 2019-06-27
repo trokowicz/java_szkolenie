3689ed7ff39658213299d4d331d9916062eb60f9
trokowicz <51273895+trokowicz@users.noreply.github.com>
2019-06-27 22:51:25 +0200

    Lambda and max element in the collection - Group creation tests

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,7 +1,7 @@
 package pl.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.GroupData;
 
 import java.util.HashSet;
@@ -13,18 +13,12 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    GroupData group = new GroupData("test2", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
