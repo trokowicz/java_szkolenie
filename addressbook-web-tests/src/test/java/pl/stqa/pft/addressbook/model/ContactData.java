a0c4ad76d9254b0c8d3987d04d6c11755c08d875
trokowicz <51273895+trokowicz@users.noreply.github.com>
2019-07-03 22:03:01 +0200

    Added equals method in ContactData

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/ContactData.java
@@ -65,6 +65,21 @@ public class ContactData {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactData that = (ContactData) o;
+        return id == that.id &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+
+    @Override
     public String toString() {
         return "ContactData{" +
                 "id='" + id + '\'' +
