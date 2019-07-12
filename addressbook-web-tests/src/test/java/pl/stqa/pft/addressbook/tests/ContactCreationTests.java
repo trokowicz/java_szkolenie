a4f5c0a24e49c607d34cfb43a06680c8000d4250
trokowicz <51273895+trokowicz@users.noreply.github.com>
2019-07-13 00:07:43 +0200

    Loading test data from a file

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,8 +4,10 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.ContactData;
 import pl.stqa.pft.addressbook.model.Contacts;
-
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,19 +18,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new ContactData()
-            .withLastName("Lastname1").withFirstName("Firstname1").withTitle("Mr").withEmail("email@test1.com").withGroup("test1")});
-    list.add(new Object[] {new ContactData()
-            .withLastName("Lastname2").withFirstName("Firstname2").withTitle("Mr").withEmail("email@test2.com").withGroup("test2")});
-    list.add(new Object[] {new ContactData()
-            .withLastName("Lastname3").withFirstName("Firstname3").withTitle("Mr").withEmail("email@test3.com").withGroup("test3")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withLastName(split[0]).withFirstName(split[1]).withEmail(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
   @Test(dataProvider = "validContacts")
-  public void testNewContact(ContactData contact) throws Exception {
+  public void testNewContact(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
     app.contact().create(contact, true);
