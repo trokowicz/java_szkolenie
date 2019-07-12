868bea84e6d91be760bcedc30e078acefec3de5f
trokowicz <51273895+trokowicz@users.noreply.github.com>
2019-07-12 23:17:27 +0200

    Test methods parameters - Data provider

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,26 +1,36 @@
 package pl.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.ContactData;
 import pl.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testNewContact() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData()
+            .withLastName("Lastname1").withFirstName("Firstname1").withTitle("Mr").withEmail("email@test1.com").withGroup("test1")});
+    list.add(new Object[] {new ContactData()
+            .withLastName("Lastname2").withFirstName("Firstname2").withTitle("Mr").withEmail("email@test2.com").withGroup("test2")});
+    list.add(new Object[] {new ContactData()
+            .withLastName("Lastname3").withFirstName("Firstname3").withTitle("Mr").withEmail("email@test3.com").withGroup("test3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testNewContact(ContactData contact) throws Exception {
     app.goTo().homePage();
-
     Contacts before = app.contact().all();
-
-    File photo = new File("src/test/resources/27.png");
-    ContactData contact = new ContactData()
-            .withFirstName("Maciej").withLastName("Kowalski").withTitle("Mr").withEmail("maciej.kowalski@gmail.io").withGroup("test1")
-            .withPhoto(photo);
     app.contact().create(contact, true);
     app.goTo().homePage();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
